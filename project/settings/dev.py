f16adecd16ee7e63d365cf75087fcd27db92ee54
Daniel Stephenson <dan.stephenson@gmail.com>
2014-04-06 12:54:26 -0600

    Initial commit for branch v1

---
+++ project/settings/dev.py
@@ -0,0 +1,61 @@
+"""Development settings and globals."""
+
+from base import *
+
+
+########## DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = True
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
+TEMPLATE_DEBUG = DEBUG
+########## END DEBUG CONFIGURATION
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'jxp9w@b$iso+&877e#_@@5yc%y3iz_#a1h-za%!_ui9doeqn7@'
+
+########## EMAIL CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+########## END EMAIL CONFIGURATION
+
+########## DATABASE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'angular_django_ecommerce',
+        'USER': 'da_ecommerce',
+        'PASSWORD': 'da_ecommerce',
+        'HOST': 'localhost'
+    }
+}
+########## END DATABASE CONFIGURATION
+
+########## CACHE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+########## END CACHE CONFIGURATION
+
+########## TOOLBAR CONFIGURATION - This requires a database
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+INTERNAL_IPS = ('127.0.0.1', 'localhost',)
+
+# See: https://github.com/django-debug-toolbar/django-debug-toolbar#installation
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
+########## END TOOLBAR CONFIGURATION
